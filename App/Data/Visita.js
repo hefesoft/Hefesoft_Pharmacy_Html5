ac98cd21ebfb30104e84957fe2ebeb8cad200975
Douglas <hefesoft@hotmail.com>
2013-08-12 21:36:56 -0500

    Planeacion

--- App/Data/Visita.js
+++ App/Data/Visita.js
@@ -6,7 +6,9 @@ define(
 ], function (global, Q, router) {
     var Visita = {
         AgregarVisita: AgregarVisita,
-        EliminarVisita: EliminarVisita
+        EliminarVisita: EliminarVisita,
+        SumarContacto : SumarContacto,      
+        cargarVisitas : cargarVisitas
     };
 
     function AgregarVisita(global, Q, Azure_Mobile_Services, visita) {
@@ -34,7 +36,42 @@ define(
         var MobileServiceClient = WindowsAzure.MobileServiceClient;
         var client = new WindowsAzure.MobileServiceClient('https://hefesoftpharmacy.azure-mobile.net/', 'kkSCbZkUqmJXuzhstBCOGgQVoWLLkr57');
         var todoItemTable = client.getTable('TM_Visita');
-        todoItemTable.del({ id: idEliminar });        
+        todoItemTable.del({ id: idEliminar });
+    }
+
+    function SumarContacto(global, id, numeroContactos) {
+        var MobileServiceClient = WindowsAzure.MobileServiceClient;
+        var client = new WindowsAzure.MobileServiceClient('https://hefesoftpharmacy.azure-mobile.net/', 'kkSCbZkUqmJXuzhstBCOGgQVoWLLkr57');
+        var todoItemTable = client.getTable('TM_Panel');
+
+        todoItemTable.update({
+            id: id,
+            ContactosActual: numeroContactos
+        });        
+    };
+
+    function cargarVisitas(global,Q,Azure_Mobile_Services,id_Usuario){
+        var deferred = Q.defer();
+        var MobileServiceClient = WindowsAzure.MobileServiceClient;
+        var client = new WindowsAzure.MobileServiceClient('https://hefesoftpharmacy.azure-mobile.net/', 'kkSCbZkUqmJXuzhstBCOGgQVoWLLkr57');
+        var todoItemTable = client.getTable('TM_Visita');
+
+        var query = todoItemTable.where({
+            id_Usuario: id_Usuario
+        })
+        .read()
+        .done(function (results) {
+             if (results.length > 0) {
+                 deferred.resolve(results);
+             }
+             else {
+                 deferred.resolve(results);
+             }
+         }, function (err) {
+             deferred.reject(new Error("Error " + err));
+         });
+
+        return deferred.promise;
     }
 
     return Visita;
--- App/viewmodels/planear.js
+++ App/viewmodels/planear.js
@@ -8,9 +8,11 @@ define([
 'durandal/system',
  'logger',
  'durandal/plugins/router',
- 'global/vars'
+ 'global/vars',
+ 'Toastr'
  ],
- function (system, logger, router) {
+ function (system, logger, router, global, toastr) {
+     var toastr = toastr;
      var VisitaDataContext;
      var elementoSeleccionado;
      var Global_Q;
@@ -18,6 +20,8 @@ define([
      var dataSourceSheduler = new kendo.data.SchedulerDataSource({
          data: []
      });
+
+
 
      var dataSourceExtensions = {
          updateField: function (e) {
@@ -31,7 +35,32 @@ define([
                      return false;
                  }
              });
+         },
+         getByField: function (e) {
+             var ds = this;
+
+             for (x in ds._data) {
+                 var item = ds._data[x];
+                 if (item[e.keyField] == e.keyValue) {
+                     var elemento = ds._data[x];
+                     return elemento;
+                 }
+             }
+         },
+         actualizar: function (e) {
+             var ds = this;
+
+             for (x in ds._data) {
+                 var item = ds._data[x];
+                 if (item[e.keyField] == e.keyValue) {
+                     var elemento = ds._data[x];
+                     elemento.set(e.updateField, e.updateValue);
+                     ds.sync();
+                     return true;
+                 }
+             }
          }
+
      };
 
      $.extend(true, kendo.data.DataSource.prototype, dataSourceExtensions);
@@ -86,16 +115,23 @@ define([
                  dataItem = dataSourceMedicos.view()[index];
                  elementoSeleccionado = dataItem;
 
-                 var dia = moment().format('d');
-
-
-                 if (dia == 6 || dia == 7) {
-                     alert('Dia no habilitado para planear');
-                     toastr.warning('Dia no permitido para planear');
+                 if (elementoSeleccionado.ContactosOriginal <= elementoSeleccionado.ContactosActual) {
+                     toastr.error('Numero de contactos', 'Numero de contactos superados');
+                     elementoSeleccionado = undefined;
                  }
+
                  else {
-                     var scheduler = $("#scheduler").data("kendoScheduler");
-                     scheduler.addEvent({ title: dataItem.Nombres + ' ' + dataItem.Apellidos });
+                     var dia = moment().format('d');
+
+
+                     if (dia == 6 || dia == 7) {
+                         alert('Dia no habilitado para planear');
+                         toastr.warning('Dia no permitido para planear');
+                     }
+                     else {
+                         var scheduler = $("#scheduler").data("kendoScheduler");
+                         scheduler.addEvent({ title: dataItem.Nombres + ' ' + dataItem.Apellidos });
+                     }
                  }
              }
          });
@@ -117,14 +153,16 @@ define([
                  resize: false
              },
              /*timezone: "America/Bogota",*/
-             views: [{
-                        /*{ type: "agenda", selected: true },*/
-                        type: "agenda",
-                        startTime: new Date(moment().add('days', 1).format('YYYY/MM/DD'))
-            
-            /* Si se deja day cuando hacen reorder en el day salva tambien es mejor crear otro de solo lectura mientras mejoran el control
-            ,"day"*/
-          }],
+             views: [
+               "day",
+                    { type: "agenda", selected: true },
+                    "month",
+                    "week",
+                    { startTime: new Date(moment().add('days', 1).format('YYYY/MM/DD')) }
+               ],
+
+
+
              dataSource: dataSourceSheduler,
              /*resources: [
              {
@@ -145,19 +183,63 @@ define([
                      Direccion_Mostrar: elementoSeleccionado.Direccion,
                      Tipo: elementoSeleccionado.Tipo,
                      Fecha: moment(e.model.start).format('YYYY/MM/DD'),
-                     Hora: moment(e.model.start).format('HH:mm')
+                     Hora: moment(e.model.start).format('HH:mm'),
+                     Anio_Entero: moment(e.model.start).format('YYYY'),
+                     Mes_Entero: moment(e.model.start).format('MM'),
+                     Dia_Entero: moment(e.model.start).format('DD'),
+                     Hora_Entero: moment(e.model.start).format('HH'),
+                     Minuto_Entero: moment(e.model.start).format('mm'),
+                     id_Usuario: 2 /* Para pruebas */
                  };
 
                  VisitaDataContext.AgregarVisita('', Global_Q, Global_Azure_Mobile_Services, visita).then(function (result) {
-                     dataSourceSheduler.updateField({ keyField: 'title', keyValue: e.model.title, updateField: 'identificadorHefesoftPharmacy', updateValue: result.id });
+                     dataSourceSheduler.actualizar({ keyField: 'title', keyValue: e.model.title, updateField: 'identificadorHefesoftPharmacy', updateValue: result.id });
+                     dataSourceSheduler.actualizar({ keyField: 'title', keyValue: e.model.title, updateField: 'IdPanel', updateValue: elementoSeleccionado.id });
+
+                     // Sumar un contacto                     
+                     VisitaDataContext.SumarContacto('', elementoSeleccionado.id, elementoSeleccionado.ContactosActual + 1);
+
+                     //Actualizar el listView
+                     dataSourceMedicos.updateField({ keyField: 'id', keyValue: elementoSeleccionado.id, updateField: 'ContactosActual', updateValue: elementoSeleccionado.ContactosActual + 1 });
+
                      console.log('Elemento agendado');
                  }, function (error) { toastr.warning(error); });
              },
              remove: function (e) {
                  console.log("Removing", e.event.identificadorHefesoftPharmacy);
                  VisitaDataContext.EliminarVisita('', Global_Q, Global_Azure_Mobile_Services, e.event.identificadorHefesoftPharmacy);
+
+                 elementoSeleccionado = dataSourceMedicos.getByField({ keyField: 'id', keyValue: e.event.IdPanel });
+
+                 VisitaDataContext.SumarContacto('', e.event.IdPanel, elementoSeleccionado.ContactosActual - 1);
+                 dataSourceMedicos.updateField({ keyField: 'id', keyValue: e.event.IdPanel, updateField: 'ContactosActual', updateValue: elementoSeleccionado.ContactosActual - 1 });
+
              }
          });
+
+         VisitaDataContext.cargarVisitas('', Global_Q, Global_Azure_Mobile_Services, 2).then(function (result) {
+             if (result.length > 0) {
+
+                 $.each(result, function (idx, record) {
+
+                     var evento = {
+                         id: 1,
+                         start: new Date(record.Anio_Entero + "/" + record.Mes_Entero + "/" + record.Dia_Entero + " " + record.Hora_Entero + ":" + "00"),
+                         end: new Date(record.Anio_Entero + "/" + record.Mes_Entero + "/" + record.Dia_Entero + " " + parseInt(record.Hora_Entero + 1) + ":" + "00"),
+                         isAllDay: false,
+                         title: record.Nombre_Mostrar,
+                         identificadorHefesoftPharmacy: record.id,
+                         IdPanel: record.IdPanel
+                     };
+
+                     dataSourceSheduler.add(evento);
+                 });
+
+             }
+             else {
+                 console.log('Sin resultados');
+             }
+         }, function (error) { toastr.warning(error); });
      }
      return planear;
  });
